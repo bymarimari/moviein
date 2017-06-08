f558e59109fb9b2a4fcfe7dd9e023fd127d6e108
Marianna Espindola <bymarimari@hotmail.com>
2017-06-08 12:11:43 +0000

    Atualização do Controller e da model Curiosidades

--- projetomoviein/application/Controller/Curiosidades.php
+++ projetomoviein/application/Controller/Curiosidades.php
@@ -20,4 +20,49 @@ class Curiosidades extends CI_Controller {
 		
 		$this->load->view('base', $dados);
 	}
-        }
+  
+  public function get(){
+			$id = $this->input->post('id', true);
+			
+			$this->load->model('Curiosidades_model');
+			
+			echo json_encode($this->Curiosidades_model->get($id));
+		}
+	
+		public function inserir(){
+		if ($this->session->has_userdata('usuario')) {
+			$usuario = $this->session->userdata('usuario');
+			if ($usuario['tipo'] == 2) {
+				$this->load->model('Curiosidades_model');
+				echo json_encode($this->Curiosidades_model->inserir($this->input->post(), $_FILES['imagem']));
+			} else {
+				echo json_encode(array('tipo' => 'fail', 'mensagem' => 'Você precisa ser administrador para executar esta ação! :P'));
+			}
+		} else {
+			echo json_encode(array('tipo' => 'fail', 'mensagem' => 'Você precisa estar logado como administrador para realizar esta ação! :P'));
+		}
+}
+
+
+		public function excluir()
+			{
+				if ($this->session->has_userdata('usuario')) {
+					$usuario = $this->session->userdata('usuario');
+					
+					if ($usuario['tipo'] == 2) {
+						$this->load->model('Curiosidades_model');
+						
+					if ($this->Curiosidades_model->excluir($this->input->post('id'))) {
+					echo json_encode(array('tipo' => 'ok', 'mensagem' => 'Excluído com sucesso!'));
+					
+				} else {
+					echo json_encode(array('tipo' => 'fail', 'mensagem' => 'Houve algum erro ao excluir esta curiosidade.'));
+				}
+			} else {
+				echo json_encode(array('tipo' => 'fail', 'mensagem' => 'Você precisa ser administrador para executar esta ação! :P'));
+			}
+		} else {
+			echo json_encode(array('tipo' => 'fail', 'mensagem' => 'Você precisa estar logado como administrador para realizar esta ação! :P'));
+		}
+	}
+}
--- projetomoviein/application/models/Curiosidades_model.php
+++ projetomoviein/application/models/Curiosidades_model.php
@@ -15,4 +15,56 @@ class Curiosidades_model extends CI_Model{
     public function get($id){
         return $this->db->get_where('curiosidade', array('id_curiosidade' => $id))->row();
     }
+	public function inserir($dados, $imagem){
+		$this->load->library('form_validation');
+		
+		$this->form_validation->set_rules('titulo', 'Título', 'max_length[70]');
+
+		if ($this->form_validation->run()) {
+		    
+		    if (!empty($imagem)) {
+		        $this->load->library('upload');
+		        $config = array();
+		        $config['upload_path'] = FCPATH.'public/img/curiosidades/';
+    			$config['allowed_types'] = 'jpg|jpeg|png';
+		        $config['file_name'] = time();
+		        $config['file_ext_tolower'] = TRUE;
+		        $this->upload->initialize($config);
+		        
+		        if ($this->upload->do_upload('imagem')) {
+		            $imagem = $this->upload->data();
+		            $dados['imagem'] = $imagem['file_name'];
+		            
+    				$this->load->library('image_moo');
+    			    $this->image_moo->load($config['upload_path'].$dados['imagem'])->resize_crop(190,280)->set_jpeg_quality(100)->save_pa('', '', TRUE);
+        		    $this->db->set($dados);
+        		    
+        		    if ($this->db->insert('curiosidade')) {
+		                return array('tipo' => 'ok', 'mensagem' => 'Inserida com sucesso!');
+        		    } else {
+        		        return array('tipo' => 'fail', 'mensagem' => 'Houve um erro ao salvar os dados');
+        		    }
+		        } else {
+		            return array('tipo' => 'fail', 'mensagem' => $this->upload->display_errors());
+		        }
+		    } else {
+		        return array('tipo' => 'fail', 'mensagem' => 'É necessário o envio da imagem.');
+		    }
+		} else {
+		    return array('tipo' => 'fail', 'mensagem' => $this->form_validation->error_string('',''));
+		}
+    }
+    
+    public function update($dados){
+        
+    }
+    
+    public function excluir($id){
+        $curiosidade = $this->get($id);
+        if (!empty($curiosidade)) {
+            unlink(FCPATH.'public/img/curiosidades/'.$curiosidade->imagem);
+            return $this->db->delete('curiosidade', array('id_curiosidade' => $curiosidade->id_curiosidade));
+        }
+    }
+    
 }
