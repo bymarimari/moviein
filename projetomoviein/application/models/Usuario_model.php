652d303a83993243574cc7fc6fc307c156e6d186
Julianna Mello <callmejuh@gmail.com>
2017-06-09 00:00:07 +0000

    Criação do Controller e Model Usuario

---
+++ projetomoviein/application/models/Usuario_model.php
@@ -0,0 +1,17 @@
+<?php
+
+class Usuario_model extends CI_Model{
+    private $nome;
+    
+    public function inserir(){
+        
+    }
+
+    public function get(){
+
+    }
+    
+    public function autentica(){
+    
+    }
+}
