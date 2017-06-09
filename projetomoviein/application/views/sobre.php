da47c3b66b516f66c40e63d1b93f3425d7c94dd6
Marianna Espindola <bymarimari@hotmail.com>
2017-06-09 13:21:22 +0000

    Criação do Controller e da View Sobre

---
+++ projetomoviein/application/views/sobre.php
@@ -0,0 +1,49 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+            <div class="container">
+            <h3>Sobre</h3>
+
+                <div class="sobre row">
+                    
+                </div>
+                  
+                <div class="sobre row">           
+                 <h4>Colaboradores</h4>
+                        
+                            
+                    <div class="sobre row">
+                    <div class="sobre-imagem col s12 m3">
+                        <img src="<?= $this->public; ?>" class="circle responsive-img">
+                    </div>
+                    <div class="sobre-texto col s12 m9">
+                        <h5>Julianna</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris cursus vel orci et semper. Curabitur lobortis posuere turpis, ut varius turpis vulputate et. Vivamus eu placerat ipsum. Phasellus laoreet turpis ut imperdiet dignissim. Aenean facilisis neque neque, varius luctus orci volutpat a. Cras luctus lobortis urna vel pulvinar. Suspendisse in sapien enim. Pellentesque justo velit, pellentesque finibus tincidunt eu, efficitur in mi.</p>
+                    </div>
+                      
+                        
+                        
+                </div>
+                    <div class="sobre row">
+                    <div class="sobre-imagem col s12 m3">
+                        <img src="<?= $this->public; ?>"class="circle responsive-img">
+                    </div>
+                    <div class="sobre-texto col s12 m9">
+                        <h5>Lucas</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris cursus vel orci et semper. Curabitur lobortis posuere turpis, ut varius turpis vulputate et. Vivamus eu placerat ipsum. Phasellus laoreet turpis ut imperdiet dignissim. Aenean facilisis neque neque, varius luctus orci volutpat a. Cras luctus lobortis urna vel pulvinar. Suspendisse in sapien enim. Pellentesque justo velit, pellentesque finibus tincidunt eu, efficitur in mi.</p>
+                    </div>
+                        
+                    </div>
+
+                <div class="sobre row">
+                    <div class="sobre-imagem col s12 m3">
+                        <img src="<?= $this->public; ?>"class="circle responsive-img">
+                    </div>
+                    <div class="sobre-texto col s12 m9">
+                        <h5>Marianna</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris cursus vel orci et semper. Curabitur lobortis posuere turpis, ut varius turpis vulputate et. Vivamus eu placerat ipsum. Phasellus laoreet turpis ut imperdiet dignissim. Aenean facilisis neque neque, varius luctus orci volutpat a. Cras luctus lobortis urna vel pulvinar. Suspendisse in sapien enim. Pellentesque justo velit, pellentesque finibus tincidunt eu, efficitur in mi.</p>
+                    </div>
+                       
+                </div>
+                 </div>
+               
+            </div>
