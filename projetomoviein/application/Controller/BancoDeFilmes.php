8dacd94cc721d0651dbb5f61541e0cebcc246f64
Lucas Figueiredo <lucas-fj@hotmail.com>
2017-06-12 02:54:23 +0000

    Alterações em Controller, model e View do Banco de filmes

--- projetomoviein/application/Controller/BancoDeFilmes.php
+++ projetomoviein/application/Controller/BancoDeFilmes.php
@@ -28,4 +28,63 @@ class BancoDeFilmes extends CI_Controller {
 		
 		echo json_encode($this->Filmes_model->get($id));
 	}
+	
+	public function inserir(){
+		if ($this->session->has_userdata('usuario')) {
+			$usuario = $this->session->userdata('usuario');
+			if ($usuario['tipo'] == 2) {
+				$this->load->model('Filmes_model');
+				echo json_encode($this->Filmes_model->inserir($this->input->post(), $_FILES['imagem']));
+			} else {
+				echo json_encode(array('tipo' => 'fail', 'mensagem' => 'Você precisa ser administrador para executar esta ação! :P'));
+			}
+		} else {
+			echo json_encode(array('tipo' => 'fail', 'mensagem' => 'Você precisa estar logado como administrador para realizar esta ação! :P'));
+		}
+	}
+
+	
+	public function excluir()
+			{
+				if ($this->session->has_userdata('usuario')) {
+					$usuario = $this->session->userdata('usuario');
+					
+					if ($usuario['tipo'] == 2) {
+						$this->load->model('Filmes_model');
+						
+					if ($this->Filmes_model->excluir($this->input->post('id'))) {
+					echo json_encode(array('tipo' => 'ok', 'mensagem' => 'Excluído com sucesso!'));
+					
+				} else {
+					echo json_encode(array('tipo' => 'fail', 'mensagem' => 'Houve algum erro ao excluir este filme.'));
+				}
+			} else {
+				echo json_encode(array('tipo' => 'fail', 'mensagem' => 'Você precisa ser administrador para executar esta ação! :P'));
+			}
+		} else {
+			echo json_encode(array('tipo' => 'fail', 'mensagem' => 'Você precisa estar logado como administrador para realizar esta ação! :P'));
+		}
+	}
+
+	
+	public function adicionar()
+	{
+		if ($this->session->has_userdata('usuario')) {
+			$usuario = $this->session->userdata('usuario');
+			
+			if ($usuario['tipo'] == 1) {
+				$this->load->model('Filmes_model');
+				
+				if ($this->Filmes_model->adicionar($this->input->post('id'), $usuario['email'])) {
+					echo json_encode(array('tipo' => 'ok', 'mensagem' => 'Adicionado com sucesso!'));
+				} else {
+					echo json_encode(array('tipo' => 'fail', 'mensagem' => 'Houve algum erro ao adicionar este filme.'));
+				}
+			} else {
+				echo json_encode(array('tipo' => 'fail', 'mensagem' => 'Você precisa ser um usuário cadastrado para executar esta ação! :P'));
+			}
+		} else {
+			echo json_encode(array('tipo' => 'fail', 'mensagem' => 'Você precisa estar logado para realizar esta ação! :P'));
+		}
+	}
 }
